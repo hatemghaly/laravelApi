25038984e390600eee27edd4dca730aa2f07d79e
Hatem Mohammed Ghaly <hatem230@yahoo.com>
2018-01-11 17:41:14 +0200

    2 change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hatem Web
                 </div>
 
                 <div class="links">
